46a16d544e935069d7471c2014d275514dffa75f
Valerii <valerii_chubar@epam.com>
2020-05-21 21:42:42 +0300

    test

--- src/vis_client_example.py
+++ src/vis_client_example.py
@@ -28,7 +28,8 @@ logger = logging.getLogger(__name__)
 VIS_URL = os.getenv("VIS_URL", "wss://wwwivi:8088/")
 
 # Go to https://webhook.site/#/ copy and paste server link to HTTP_REQUEST_RECEIVER_URL
-HTTP_REQUEST_RECEIVER_URL = os.getenv("HTTP_REQUEST_RECEIVER_URL")
+HTTP_REQUEST_RECEIVER_URL = "https://webhook.site/de6eedb7-a916-4974-8e96-96b279b29bba"
+#HTTP_REQUEST_RECEIVER_URL = os.getenv("HTTP_REQUEST_RECEIVER_URL")
 
 DATA_SENDING_DELAY = 2
 WAIT_TIMEOUT = 5
